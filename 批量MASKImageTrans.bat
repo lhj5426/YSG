6c358b455c2d74480981c4f827515e3db2ad7db2
lhj5426 <30548000+lhj5426@users.noreply.github.com>
2025-03-16 11:07:16 +0800

    Add files via upload

---
+++ 批量MASKImageTrans.bat
@@ -0,0 +1,13 @@
+@echo off
+REM 激活 Conda 环境
+call conda activate yolov11
+
+REM 运行 Python 脚本，支持拖放文件路径传递
+python piliangMASKImageTrans.py %*
+
+REM 取消激活 Conda 环境
+call conda deactivate
+
+REM 暂停以便查看结果
+echo 按任意键继续...
+pause >nul
